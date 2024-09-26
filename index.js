4c5cb6a27a5155222f15805aad6fdfb486fdc074
Victoria <shevvictoryia@gmail.com>
2024-09-26 23:13:37 +0300

    feat: add delete post method

--- index.js
+++ index.js
@@ -88,6 +88,14 @@ app.patch("/posts/:id", (req, res) => {
 });
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const postInd = posts.findIndex((post) => post.id === id);
+  if (postInd === -1)
+    return res.status(404).json({ message: "Post not found" });
+  posts.splice(postInd, 1);
+  res.json({ message: "Post deleted" });
+});
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
